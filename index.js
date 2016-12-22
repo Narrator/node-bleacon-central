7da57872ee2b75c04d70634cd8452c67c665884f
Kaushik Subramaniam <kaushik.subman@gmail.com>
2016-12-22 02:14:52 -0600

    scheduled emitting

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Bleacon = require('bleacon');
 //replace localhost with your server's IP;
 var socket = require('socket.io-client')('http://192.168.0.104:3000/scanner');
+var schedule = require('node-schedule');
+var dateFormat = require('dateformat');
 
 //Bleacon.startScanning(); // scan for any bleacons
 
@@ -29,15 +31,23 @@ if(process.env.BEACON_COLOR === "red") {
   process.exit();
 }
 
-
+var emittingData = {};
 Bleacon.on("discover", function (bleacon) {
-  socket.emit('deviceData', {
+  emittingData = {
     name: beacon.name,
-    rssi: bleacon.rssi
-  });
+    rssi: bleacon.rssi,
+    time: dateFormat(new Date(), "hh:MM:ss")
+  };
 });
 
 socket.on('connect', function(){
   console.log('connected to server');
   Bleacon.startScanning(beacon.uuid, beacon.major, beacon.minor); // scan for bleacons with a particular uuid. major, and minor
+});
+
+var rule = new schedule.RecurrenceRule();
+rule.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+
+schedule.scheduleJob(rule, function () {
+  socket.emit('deviceData', emittingData);
 });
